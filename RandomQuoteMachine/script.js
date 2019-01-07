5dbc60ee7949a401feeb851e0a008b9cc0cd3cc5
Oleg Abalonski <o_abalonski@yahoo.com>
2019-01-07 00:10:05 -0500

    Finish random quote and color logic

--- RandomQuoteMachine/script.js
+++ RandomQuoteMachine/script.js
@@ -1,43 +1,52 @@
-// https://quotesondesign.com/api-v4-0/
+const backgroundColors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+var currentColorIndex = Math.floor(Math.random() * (backgroundColors.length));
 
+function getNextColor() {
+  // if (currentColorIndex > backgroundColors.length - 1) {
+  //   currentColorIndex -= backgroundColors.length - 1;
+  // }
+  // currentColorIndex++;
+  // let nextColor = backgroundColors[currentColorIndex];
 
-var newQuote = "Many years later, as he faced the firing squad, Colonel Aureliano Buendía was to remember that distant afternoon when his father took him to discover ice"
-var newAuthor = "Gabriel García Márquez"
+  let randomIndex;
+  do {
+    randomIndex = Math.floor(Math.random() * (backgroundColors.length));
+  } while (randomIndex == currentColorIndex);
+
+  return backgroundColors[randomIndex];
+}
 
 $(document).ready(function() {
 
-  // $('#new-quote').click(function() {
-  //
-  //   $('#text').fadeOut();
-  //   $('#author').fadeOut();
-  //
-  //   $('#text').html(newQuote);
-  //   $('#author').html(newAuthor);
-  //
-  //   $('#text').fadeIn();
-  //   $('#author').fadeIn();
-  //
-  // });
+  $.fn.getNewQuote = function() {
+    // https://quotesondesign.com/api-v4-0/
+    const apiURL = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
 
-  //http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1
-
-  $('#new-quote').on('click', function(e) {
-    e.preventDefault();
     $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-      datatype: 'jsonp',
+      url: apiURL,
       success: function(data) {
-        let post = data.shift(); // The data is an array of posts. Grab the first one.
+        let post = data.shift();
         let text = post.content;
         text = text.replace("<p>", "");
-        text = text.replace("</p>", "");
+        text = text.replace("<\/p>\n", "");
         let author = post.title;
 
         $("#text").html(text);
         $("#author").html(author);
+
+        $('#quote-box').fadeIn();
       },
       cache: false
     });
+  };
+
+  $('#new-quote').on('click', function(getNewQuote) {
+    $('#quote-box').fadeOut()
+    getNewQuote.preventDefault();
+    $('#main').animate({backgroundColor: getNextColor()}, 'slow');
+    $().getNewQuote();
   });
 
+  $('#main').animate({backgroundColor: backgroundColors[currentColorIndex]}, 'slow');
+  $().getNewQuote()
 });
