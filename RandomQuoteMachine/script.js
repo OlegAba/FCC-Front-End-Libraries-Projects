d6332e65283f7076f4f47998ed3d4401ebe30cca
Oleg Abalonski <o_abalonski@yahoo.com>
2019-01-07 22:42:09 -0500

    Finish styling

--- RandomQuoteMachine/script.js
+++ RandomQuoteMachine/script.js
@@ -1,13 +1,7 @@
 const backgroundColors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 var currentColorIndex = Math.floor(Math.random() * (backgroundColors.length));
 
-function getNextColor() {
-  // if (currentColorIndex > backgroundColors.length - 1) {
-  //   currentColorIndex -= backgroundColors.length - 1;
-  // }
-  // currentColorIndex++;
-  // let nextColor = backgroundColors[currentColorIndex];
-
+function getNewColor() {
   let randomIndex;
   do {
     randomIndex = Math.floor(Math.random() * (backgroundColors.length));
@@ -22,14 +16,17 @@ $(document).ready(function() {
     // https://quotesondesign.com/api-v4-0/
     const apiURL = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
 
+    // TODO: Get new quote if quote exceeds a certain amount of character
+    // TODO: Create next quote variable that is intialized when first quote is recieved. This way there is no delay when next quote button is clicked
+    // TODO: Append quotes in before and after text
     $.ajax( {
       url: apiURL,
       success: function(data) {
         let post = data.shift();
         let text = post.content;
-        text = text.replace("<p>", "");
-        text = text.replace("<\/p>\n", "");
-        let author = post.title;
+        text = text.replace("<p>", "“");
+        text = text.replace("<\/p>\n", "”");
+        let author = '— ' + post.title;
 
         $("#text").html(text);
         $("#author").html(author);
@@ -43,7 +40,7 @@ $(document).ready(function() {
   $('#new-quote').on('click', function(getNewQuote) {
     $('#quote-box').fadeOut()
     getNewQuote.preventDefault();
-    $('#main').animate({backgroundColor: getNextColor()}, 'slow');
+    $('#main').animate({backgroundColor: getNewColor()}, 'slow');
     $().getNewQuote();
   });
 
