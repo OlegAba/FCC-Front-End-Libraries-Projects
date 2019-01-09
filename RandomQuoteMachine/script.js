8a2c799efd234c503089fca56bb73982a89b25d0
Oleg Abalonski <o_abalonski@yahoo.com>
2019-01-08 22:26:21 -0500

    Fix api character limit bug

--- RandomQuoteMachine/script.js
+++ RandomQuoteMachine/script.js
@@ -1,8 +1,11 @@
-const backgroundColors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+const backgroundColors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12',
+                          '#e74c3c', '#9b59b6', '#FB6964', '#342224',
+                          "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 var currentColorIndex = Math.floor(Math.random() * (backgroundColors.length));
 
 function getNewColor() {
-  let randomIndex;
+  let randomIndex = null;
+
   do {
     randomIndex = Math.floor(Math.random() * (backgroundColors.length));
   } while (randomIndex == currentColorIndex);
@@ -14,33 +17,46 @@ $(document).ready(function() {
 
   $.fn.getNewQuote = function() {
     // https://quotesondesign.com/api-v4-0/
-    const apiURL = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
+    const apiURL = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=5"
 
-    // TODO: Get new quote if quote exceeds a certain amount of character (Twitter max chars 280)
-    // TODO: Create next quote variable that is intialized when first quote is recieved. This way there is no delay when next quote button is clicked
     $.ajax( {
       url: apiURL,
       success: function(data) {
-        let post = data.shift();
-        let text = post.content;
-        text = text.replace("<p>", "“");
-        text = text.replace("<\/p>\n", "”");
-        text.replace(/&#8217;/g, "\u2019");
-        //Remove <br> from quote
-        let author = '— ' + post.title;
+
+        let text = null;
+        let author = null;
+
+        for (let index = 0; index < data.length; index++) {
+
+          let post = data[index];
+
+          text = post.content;
+          text = text.replace("<p>", "“");
+          // Check if space is before and remove it3
+          text = text.replace("<\/p>\n", "”");
+
+          author = '— ' + post.title;
+
+          let textAndAuthor = text + ' ' + author;
+
+          let twitterSubjectLength = '#Design Quote'.split('').length;
+          let twitterCharLimit = 280;
+          if ((textAndAuthor.split('').length + twitterSubjectLength) < twitterCharLimit) {
+            break;
+          }
+        }
 
         $("#text").html(text);
         $("#author").html(author);
 
         let displayedText = document.querySelector("#text").innerText;
         let displayedAuthor = document.querySelector("#author").innerText;
-        let textAndAuthor = displayedText + ' ' + displayedAuthor;
+        let displayedTextAndAuthor = displayedText + ' ' + displayedAuthor;
 
-        let twitterText = encodeURIComponent(textAndAuthor);
-        let tweetURL = "https://twitter.com/intent/tweet?hashtags=DesignQuotes&text=" + twitterText;
+        let tweetURL = "https://twitter.com/intent/tweet?hashtags=DesignQuotes&text=" + encodeURIComponent(displayedTextAndAuthor);
         $("#tweet-quote").attr("href", tweetURL);
 
-        let mailToURL = "mailto:?subject=Design Quote&body=" + displayedText + ' ' + displayedAuthor;
+        let mailToURL = "mailto:?subject=Design Quote&body=" + displayedTextAndAuthor;
         $("#email-btn").attr("href", mailToURL);
 
         $('#quote-box').fadeIn();
@@ -50,7 +66,7 @@ $(document).ready(function() {
   };
 
   $('#new-quote').on('click', function(getNewQuote) {
-    $('#quote-box').fadeOut()
+    $('#quote-box').fadeOut();
     getNewQuote.preventDefault();
     $('#main').animate({backgroundColor: getNewColor()}, 'slow');
     $().getNewQuote();
